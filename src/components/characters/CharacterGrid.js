05c44a701738fbccb7551e8f0b175b039e5a58ca
Atssini Cabrera <atssini23@yahoo.com>
2021-01-21 16:19:05 -0800

    spinner gif

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,10 @@
 import React from "react";
 import CharacterItem from "./CharacterItem";
+import Spinner from "../ui/Spinner";
 
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
-    <h2>Loading...</h2>
+    <Spinner />
   ) : (
     <section className="cards">
       {items.map((item) => (
